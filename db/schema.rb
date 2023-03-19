bbe127fd42ee68edd6a18ed32c74d92e83934cfa
Joshua <joshsmayhew@gmail.com>
2023-03-19 11:47:47 +0700

    migrate room table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_19_043731) do
+ActiveRecord::Schema.define(version: 2023_03_19_044538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,17 @@ ActiveRecord::Schema.define(version: 2023_03_19_043731) do
     t.string "nationality"
     t.string "passport_number"
     t.date "date_of_birth"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "type"
+    t.text "description"
+    t.integer "capacity"
+    t.integer "price_per_night"
+    t.boolean "is_available"
+    t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
