e90bd7ffbc1aac3edb23c59a2b22fe96c7db7720
Joshua <joshsmayhew@gmail.com>
2023-03-20 10:33:24 +0700

    set up fallback controller to allow for client side routing in rails app production build

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  
-  resources :users
-  resources :hotels
-  resources :bookings
-  resources :rooms
-  resources :guests
+  namespace :api do
+    resources :users
+    resources :hotels
+    resources :bookings
+    resources :rooms
+    resources :guests
+  end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
