88c099e031ae0dd587201c978f0a1855f21b0af7
Joshua <joshsmayhew@gmail.com>
2023-03-22 11:57:07 +0700

    add destory method to sessions controller and edit create method to handle login and logout functionality

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
+  before_action :authorize, only: [:destroy]
   def create
     user = User.find_by(email: params[:email])
+
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :created
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    head :no_content
   end
 end
