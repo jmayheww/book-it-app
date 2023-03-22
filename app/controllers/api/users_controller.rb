9aae565bd84f0fea8c31ebe6170a6712da84acb1
Joshua <joshsmayhew@gmail.com>
2023-03-22 11:32:10 +0700

    create post /login route and create method in userscontroller to handle new user signup, sessionscontroller create method to handle login and user auth

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,16 @@
 class Api::UsersController < ApplicationController
   include ApiResource
 
+  def create
+    user = User.create!(user_params)
+    render json: user, status: :created
+  end
+
   private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 
   def model
     User
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users do
-      get 'guest', to: 'guests#show_user_guest'
-    end
+    resources :users
+
     resources :hotels, only: %i[index show], shallow: true do
       resources :rooms, only: %i[index show]
     end
     resources :bookings
     resources :rooms
+    resources :guests
+    post '/login', to: 'session#create'
   end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
