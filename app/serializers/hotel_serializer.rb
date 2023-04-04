28128ec04dcf0c56503dc03b5a0e97b6ab82c8ca
Joshua <joshsmayhew@gmail.com>
2023-04-04 13:07:08 +0700

    edit hotel serializer to include has_many :rooms association, render rooms as nested json in hotel object and render in ViewHotelPage, rooms assigned to hotelRooms variable

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -1,3 +1,4 @@
 class HotelSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :image_url, :description, :rating
+  has_many :rooms
 end
--- client/src/pages/ViewHotelPage.js
+++ client/src/pages/ViewHotelPage.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from "../styles";
 
-function ViewHotel({ hotels }) {
+function ViewHotelPage({ hotels }) {
   const { hotelId } = useParams();
-
+  const [showRooms, setShowRooms] = useState(false);
   const viewHotel = hotels.find((hotel) => hotel.id === parseInt(hotelId));
+  const hotelRooms = viewHotel.rooms;
+  console.log("hotelRooms: ", hotelRooms);
+  console.log("viewHotel: ", viewHotel);
+  const renderRooms = hotelRooms.map((room) => {
+    return <div className="room-card">{room.description}</div>;
+  });
 
+  function handleRoomsClick() {
+    setShowRooms(!showRooms);
+  }
   return viewHotel ? (
     <>
-      <div>Test</div>
-      <h1>{viewHotel.name}</h1>
+      <div className="hotel-info">
+        <h1>{viewHotel.name}</h1>
+        <Button onClick={() => handleRoomsClick()}>Show Rooms</Button>
+      </div>
+      {showRooms ? <div className="rooms-list">{renderRooms}</div> : ""}
     </>
   ) : (
     <p>Loading...</p>
   );
 }
-export default ViewHotel;
+export default ViewHotelPage;
