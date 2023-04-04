4808ab2ecf7994c76fa86e128ac7d3db7d3c5d50
Joshua <joshsmayhew@gmail.com>
2023-04-04 13:44:44 +0700

    add styled components to RoomCard and display attributes

---
+++ client/src/components/RoomCard.js
@@ -0,0 +1,97 @@
+import React from "react";
+import styled from "styled-components";
+
+function RoomCard({ room }) {
+  const {
+    id,
+    room_title,
+    description,
+    price_per_night,
+    image_url,
+    is_available,
+    max_guests,
+  } = room;
+
+  return (
+    <RoomCardWrapper>
+      <ImageWrapper>
+        <RoomImage src={image_url} alt={`${room_title}`} />
+      </ImageWrapper>
+      <RoomContent>
+        <RoomTitle>{room_title}</RoomTitle>
+        <RoomDescription>{description}</RoomDescription>
+        <RoomPrice>${price_per_night} per night</RoomPrice>
+        <RoomDetails>
+          <MaxGuests>Max guests: {max_guests}</MaxGuests>
+          <Availability>
+            {is_available ? "Available" : "Not Available"}
+          </Availability>
+        </RoomDetails>
+      </RoomContent>
+    </RoomCardWrapper>
+  );
+}
+
+const RoomCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  width: 100%;
+  max-width: 400px;
+  margin-bottom: 16px;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+`;
+
+const RoomImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+`;
+
+const RoomContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 16px;
+`;
+
+const RoomTitle = styled.h3`
+  margin: 0;
+  font-size: 1.2rem;
+  color: #49beb7;
+`;
+
+const RoomDescription = styled.p`
+  font-size: 0.9rem;
+  color: #6c757d;
+`;
+
+const RoomPrice = styled.span`
+  font-weight: bold;
+  color: #f67280;
+`;
+
+const RoomDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+const MaxGuests = styled.span`
+  font-size: 0.9rem;
+  color: #6c757d;
+`;
+
+const Availability = styled.span`
+  font-size: 0.9rem;
+  color: ${(props) => (props.available ? "#49beb7" : "#f67280")};
+`;
+
+export default RoomCard;
--- client/src/pages/ViewHotelPage.js
+++ client/src/pages/ViewHotelPage.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Button } from "../styles";
+import RoomCard from "../components/RoomCard";
 
 function ViewHotelPage({ hotels }) {
   const { hotelId } = useParams();
   const [showRooms, setShowRooms] = useState(false);
-  const viewHotel = hotels.find((hotel) => hotel.id === parseInt(hotelId));
-  const hotelRooms = viewHotel.rooms;
-  console.log("hotelRooms: ", hotelRooms);
-  console.log("viewHotel: ", viewHotel);
-  const renderRooms = hotelRooms.map((room) => {
-    return <div className="room-card">{room.description}</div>;
-  });
 
+  // find hotel by matching hotel id with hotelId from useParams
+  const viewHotel = hotels.find((hotel) => hotel.id === parseInt(hotelId));
+
+  // assign rooms to hotelRooms variable, conditional chaining to prevent rooms undefined error
+  const hotelRooms = viewHotel?.rooms;
+
+  // conditional rendering of rooms list
   function handleRoomsClick() {
     setShowRooms(!showRooms);
   }
@@ -20,9 +21,19 @@ function ViewHotelPage({ hotels }) {
     <>
       <div className="hotel-info">
         <h1>{viewHotel.name}</h1>
-        <Button onClick={() => handleRoomsClick()}>Show Rooms</Button>
+        <Button onClick={() => handleRoomsClick()}>
+          {!showRooms ? "Show Rooms" : "Hide Rooms"}
+        </Button>
       </div>
-      {showRooms ? <div className="rooms-list">{renderRooms}</div> : ""}
+      {showRooms ? (
+        <div className="rooms-list">
+          {hotelRooms.map((hotel) => {
+            return <RoomCard key={hotel.id} room={hotel} />;
+          })}
+        </div>
+      ) : (
+        ""
+      )}
     </>
   ) : (
     <p>Loading...</p>
