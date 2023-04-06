29bbf6d54c9769bf3ab58e8bf17b3210fdd376de
Joshua <joshsmayhew@gmail.com>
2023-04-06 13:08:47 +0700

    create rails validations in bookings model checking for user uniqueness and guests are within max guest size of room, create create action in bookings controller taking strong bookings params and creating new booking object

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,9 +1,22 @@
 class Api::BookingsController < ApplicationController
   include ApiResource
 
+  def create
+    new_booking = Booking.create!(booking_params)
+    render json: new_booking, status: :created
+  end
+
   private
 
   def model
     Booking
+  end
+
+  def booking_params
+    params.permit(:user_id, :room_id, :number_of_guests, :check_in, :check_out)
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates_uniqueness_of :user_id, scope: :room_id, message: 'You have already booked this room'
+
+  validate :guests_within_limit
+
+  private
+
+  def guests_within_limit
+    return unless number_of_guests.present? && room.present?
+    return unless number_of_guests > room.number_of_guests
+
+    errors.add(:number_of_guests, "exceeds the room's guest limit")
+  end
 end
