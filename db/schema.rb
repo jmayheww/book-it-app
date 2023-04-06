3ca050483f2d3f1e0be1d3afb50247c675f02d0f
Joshua <joshsmayhew@gmail.com>
2023-04-06 11:24:46 +0700

    create new migration add column number_of_guests to bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,66 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_403_095_707) do
+ActiveRecord::Schema.define(version: 2023_04_06_042315) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bookings', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.date 'check_in'
-    t.date 'check_out'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'room_id', null: false
-    t.index ['room_id'], name: 'index_bookings_on_room_id'
-    t.index ['user_id'], name: 'index_bookings_on_user_id'
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "check_in"
+    t.date "check_out"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "room_id", null: false
+    t.integer "number_of_guests"
+    t.index ["room_id"], name: "index_bookings_on_room_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
-  create_table 'hotels', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.text 'description'
-    t.string 'image_url'
-    t.integer 'rating'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'website'
-    t.string 'phone'
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.string "image_url"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "website"
+    t.string "phone"
   end
 
-  create_table 'rooms', force: :cascade do |t|
-    t.string 'room_title'
-    t.text 'description'
-    t.integer 'price_per_night'
-    t.integer 'max_guests'
-    t.boolean 'is_available'
-    t.string 'image_url'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'hotel_id', null: false
-    t.index ['hotel_id'], name: 'index_rooms_on_hotel_id'
+  create_table "rooms", force: :cascade do |t|
+    t.string "room_title"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "max_guests"
+    t.boolean "is_available"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "hotel_id", null: false
+    t.index ["hotel_id"], name: "index_rooms_on_hotel_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'phone_number'
-    t.string 'address'
-    t.string 'city'
-    t.string 'state'
-    t.string 'country'
-    t.string 'passport_number'
-    t.integer 'age'
-    t.date 'date_of_birth'
-    t.string 'nationality'
-    t.string 'avatar_url'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "passport_number"
+    t.integer "age"
+    t.date "date_of_birth"
+    t.string "nationality"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'bookings', 'rooms'
-  add_foreign_key 'bookings', 'users'
-  add_foreign_key 'rooms', 'hotels'
+  add_foreign_key "bookings", "rooms"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "rooms", "hotels"
 end
