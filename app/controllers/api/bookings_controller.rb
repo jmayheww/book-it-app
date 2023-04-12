2e5a703c2e5be15f282806c538b446eea4328d81
Joshua <joshsmayhew@gmail.com>
2023-04-12 12:26:55 +0700

    refactor bookings controller to handle authorization and variable assignment with before_action macro, set only for update and destroy actions set current user and booking and assign to instance variables and authorize actions check if user id is equal to booking id before authorizing update and destroy actions

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,35 +1,25 @@
 class Api::BookingsController < ApplicationController
   include ApiResource
+  before_action :set_current_user, only: %i[update destroy]
+  before_action :set_booking, only: %i[update destroy]
+  before_action :authorize_user, only: %i[update destroy]
 
   rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record_resp
   rescue_from ActiveRecord::RecordNotUnique, with: :render_unique_violation_response
+
   def create
     new_booking = Booking.create!(booking_params)
     render json: new_booking, status: :created
   end
 
   def update
-    current_user = User.find_by(id: session[:user_id])
-    booking = Booking.find(params[:id])
-
-    if current_user.id == booking.user_id
-      booking.update!(booking_params)
-      render json: booking, status: :ok
-    else
-      render json: { error: 'Not authorized' }, status: :unauthorized
-    end
+    @booking.update!(booking_params)
+    render json: @booking, status: :ok
   end
 
   def destroy
-    current_user = User.find_by(id: session[:user_id])
-    booking = Booking.find(params[:id])
-
-    if current_user.id == booking.user_id
-      booking.destroy
-      render json: { message: 'Booking deleted' }, status: :ok
-    else
-      render json: { error: 'Not authorized' }, status: :unauthorized
-    end
+    @booking.destroy
+    render json: { message: 'Booking deleted' }, status: :ok
   end
 
   private
@@ -40,6 +30,20 @@ class Api::BookingsController < ApplicationController
 
   def booking_params
     params.permit(:user_id, :room_id, :number_of_guests, :check_in, :check_out)
+  end
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def authorize_user
+    return if @current_user.id == @booking.user_id
+
+    render json: { error: 'Not authorized' }, status: :unauthorized
   end
 
   def render_unique_violation_response
